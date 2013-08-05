877ce3d769020308fe3b1874b4903603ec738ce5
Peter Graham <peterghm@gmail.com>
2013-08-04 21:39:21 -0700

    Slightly better tests

--- test/fluid_image_grid_spec.js
+++ test/fluid_image_grid_spec.js
@@ -3,11 +3,19 @@ describe("FluidImageGrid", function() {
     // TODO - put this in a fixture
     $grid = $("<div class='fluid-image-grid'><div class='fluid-image-grid-inner'></div></div>");
     $grid.appendTo("body");
+
+    window.FluidImageGrid.init();
   });
 
-  describe("#init", function() {
+  context("on window resize", function() {
     it("does not raise any errors", function() {
-      window.FluidImageGrid.init();
+      $(window).trigger("resize");
+    });
+  });
+
+  context("on scroll", function() {
+    it("does not raise any errors", function() {
+      $(window).trigger("scroll");
     });
   });
 });

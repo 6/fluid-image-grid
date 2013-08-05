7e93a99616ebdc7eacf2a2cccab6cd1d6be3bf8b
Peter Graham <peterghm@gmail.com>
2013-08-04 23:45:57 -0700

    Add all the data attributes to grid div

--- test/fluid_image_grid_spec.js
+++ test/fluid_image_grid_spec.js
@@ -1,7 +1,7 @@
 describe("FluidImageGrid", function() {
   beforeEach(function() {
     // TODO - put this in a fixture
-    $grid = $("<div class='fluid-image-grid'><div class='fluid-image-grid-inner'></div></div>");
+    $grid = $("<div class='fluid-image-grid' data-row-height='180' data-ma='12' data-eca='0.1' data-minimum-result-width='80' data-isuf='0'><div class='fluid-image-grid-inner'></div></div>");
     $grid.appendTo("body");
 
     window.FluidImageGrid.init();
